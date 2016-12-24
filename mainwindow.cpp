1df09099ef6e847d38fc2eaf87042186707a2e9c
pyprism <git.pyprism@gmail.com>
2016-12-25 01:04:48 +0600

    WIP getting text from global clipboard

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,17 +2,40 @@
 #include "ui_mainwindow.h"
 #include <QClipboard>
 #include <QDebug>
+#include <QTimer>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    QString clipboard = QApplication::clipboard()->text();
-    qDebug() << clipboard;
+    QTimer *timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT(getText()));
+    timer->start(1000);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::getText()
+{
+    // QString clipboard = QApplication::clipboard()->text();
+    //if(!clipboard.isEmpty())
+    //   qDebug() << clipboard;
+
+    QClipboard *clipboard = QGuiApplication::clipboard();
+    QString originalText = clipboard->text();
+    if(!originalText.isEmpty()) {
+        if(hiren == originalText)
+            return;
+        else if(hiren != originalText){
+            hiren = originalText;
+            if(!originalText.startsWith("file:///")){
+                qDebug() << originalText;
+            }
+
+        }
+    }
 }
