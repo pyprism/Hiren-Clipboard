0e699b2221c7e989248b76d7a4d5f8696533d762
pyprism <git.pyprism@gmail.com>
2016-12-25 13:06:10 +0600

    added basic declaration

--- login.h
+++ login.h
@@ -2,6 +2,9 @@
 #define LOGIN_H
 
 #include <QMainWindow>
+#include <QDir>
+#include <QStandardPaths>
+#include <QtSql>
 
 namespace Ui {
 class Login;
@@ -15,8 +18,12 @@ public:
     explicit Login(QWidget *parent = 0);
     ~Login();
 
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::Login *ui;
+    QSqlDatabase db;
 };
 
 #endif // LOGIN_H
