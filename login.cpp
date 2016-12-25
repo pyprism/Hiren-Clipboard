dd2eab8e1b677981c052bc2c2b28cfd20476849e
pyprism <git.pyprism@gmail.com>
2016-12-25 13:06:20 +0600

    added login function

--- login.cpp
+++ login.cpp
@@ -1,14 +1,55 @@
 #include "login.h"
 #include "ui_login.h"
+#include <QDebug>
 
 Login::Login(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::Login)
 {
     ui->setupUi(this);
+    auto path = QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
+    if (path.isEmpty()) qFatal("Cannot determine settings storage location");
+    QDir d{path};
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName(d.absolutePath() + "/hiren.db");
+    db.open();
+
 }
 
 Login::~Login()
 {
     delete ui;
+}
+
+void Login::on_pushButton_clicked()
+{
+    QString username, password;
+    //bool success = false;
+    username = ui->lineEdit_username->text();
+    password = ui->lineEdit_password->text();
+
+    if(!db.isOpen()) {
+        qFatal("Database file is not openning");
+        return;
+    }
+
+    QSqlQuery query;
+    query.prepare("select * from user where username = (:username) and password = (:password)");
+    query.bindValue(":username", username);
+    query.bindValue(":password", password);
+    if(query.exec()) {
+        if (query.next())
+           {
+              qDebug() << "its working babe :D ";
+           }
+        else {
+            ui->statusbar->showMessage("Username/Password is not correct", 5000);
+            qDebug() << "Username/Password is not correct";
+        }
+
+    }
+    else {
+        qDebug() << "...";
+    }
+
 }
